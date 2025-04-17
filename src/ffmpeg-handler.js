82012229d7d8b3c202359a392d7a4a0a4dbe9b77
alex_wright <alex.wright@kallidus.com>
2025-04-18 00:42:42 +0100

    Fix duration issue

--- src/ffmpeg-handler.js
+++ src/ffmpeg-handler.js
@@ -6,10 +6,9 @@ const ffmpegPath = isDev
 	: path.join(process.resourcesPath, "ffmpeg.exe");
 const ffmpeg = require("fluent-ffmpeg");
 const { v4: uuidv4 } = require("uuid");
+const { getAudioDurationInSeconds } = require("get-audio-duration");
 
 ffmpeg.setFfmpegPath(ffmpegPath);
-
-console.log("FFmpeg path:", ffmpegPath);
 
 function sanitizeFileName(name) {
 	return name.replace(/[^a-z0-9]/gi, "_").toLowerCase();
@@ -17,23 +16,29 @@ function sanitizeFileName(name) {
 
 function createVideo({ imagePath, audioPath, resolution, audioName }) {
 	return new Promise((resolve, reject) => {
-		const outputDir = app.getPath("desktop");
-		const baseName = sanitizeFileName(audioName.replace(/\.[^/.]+$/, ""));
-		const outputPath = path.join(outputDir, `${baseName}-${uuidv4()}.mp4`);
+		getAudioDurationInSeconds(audioPath).then((duration) => {
+			const outputDir = app.getPath("desktop");
+			const baseName = sanitizeFileName(audioName.replace(/\.[^/.]+$/, ""));
+			const outputPath = path.join(outputDir, `${baseName}-${uuidv4()}.mp4`);
 
-		ffmpeg()
-			.input(imagePath)
-			.loop(1)
-			.input(audioPath)
-			.videoCodec("libx264")
-			.audioCodec("aac")
-			.audioBitrate("320k")
-			.outputOptions("-tune stillimage")
-			.outputOptions("-shortest")
-			.size(resolution)
-			.save(outputPath)
-			.on("end", () => resolve(outputPath))
-			.on("error", reject);
+			ffmpeg()
+				.input(imagePath)
+				.inputOptions(["-loop 1"])
+				.input(audioPath)
+				.videoCodec("libx264")
+				.audioCodec("aac")
+				.audioBitrate("320k")
+				.outputOptions([
+					"-tune stillimage",
+					"-pix_fmt yuv420p",
+					"-movflags +faststart",
+				])
+				.duration(duration)
+				.size(resolution)
+				.save(outputPath)
+				.on("end", () => resolve(outputPath))
+				.on("error", reject);
+		});
 	});
 }
 
